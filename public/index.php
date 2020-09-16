b763b4cde908b25a340c35f6281897dd2d85b328
alkhioz <alemendoza0197@gmail.com>
2020-09-15 19:53:53 -0500

    implementing router class

--- public/index.php
+++ public/index.php
@@ -1,17 +1,9 @@
 <?php
-/*require('../vendor/autoload.php');
+require('../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
-require_once('../controllers/controllerManager.php');
-
-$controllerManager = new controllerManager();
-
-$controller_name = isset($_REQUEST['controller']) ? $_REQUEST['controller'] : 'Default';
-$action_name = isset($_REQUEST['action']) && $_REQUEST['action'] !== "" ? $_REQUEST['action'] : 'Index';
-
-echo $_SERVER['REQUEST_METHOD'];
-
-$controllerManager->executeAction($controller_name, $action_name);
-*/
 require("../router/router.php");
+
+$router = new router();
+$router->start();
--- router/router.php
+++ router/router.php
@@ -1,9 +1,29 @@
 <?php
+require_once('../controllers/controllerManager.php');
 
-$routes = json_decode(file_get_contents("../router/routes.json"), true);
+class router{
 
-$uri = rtrim($_SERVER['REQUEST_URI'], '/');
-$uri = !array_key_exists($uri, $routes) ? '/error' : $uri;
-echo $routes[$uri]["controller"];
-echo $routes[$uri]["action"];
-echo $routes[$uri]["method"];
+    private $controllerManager;
+    private $routes;
+        
+    public function __CONSTRUCT(){
+        $this->controllerManager = new controllerManager();
+        $this->routes = json_decode(file_get_contents("../router/routes.json"), true);
+    }
+    
+    public function getUri(){
+        return rtrim($_SERVER['REQUEST_URI'], '/');
+    }
+
+    public function checkRoute(){
+        $uri = $this->getUri();
+        return !array_key_exists($uri, $this->routes) ? '/error' : $uri;
+    }
+
+    public function start(){
+        $controller_name=$this->routes[$this->checkRoute()]["controller"];
+        $action_name=$this->routes[$this->checkRoute()]["action"];
+        $this->controllerManager->executeAction($controller_name, $action_name);
+    }
+    
+}
